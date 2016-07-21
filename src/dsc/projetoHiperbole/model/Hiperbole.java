4c7f6d84d80a2fef2c46277b9ddd945cf40ca0f2
RenanOfugi <renangyn2010@hotmail.com>
2016-07-21 11:42:09 -0400

    criada classe Hiperbole para realizar calculos com variaveis de equacao prontos

---
+++ src/dsc/projetoHiperbole/model/Hiperbole.java
@@ -0,0 +1,17 @@
+package dsc.projetoHiperbole.model;
+
+public class Hiperbole implements Runnable{
+
+	private double a;
+	private double b;
+	private double c;
+	private boolean eixox;
+	private boolean eixoy;
+	
+	@Override
+	public void run() {
+		
+		
+	}
+
+}
